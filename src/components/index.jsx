0c20378f60df45d80b6a123ef9dc8e68cf33e517
Syedmoiz7 <syedzada7866@gmail.com>
2022-11-12 11:12:12 +0500

    for github login in laptop

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -179,6 +179,7 @@ function DataBase() {
                             Edit
                         </button>
 
+                            for github login in laptop
 
                     </div>
 
